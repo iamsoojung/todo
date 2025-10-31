ba27033369cc73fee1d752fe1dc81c0cdd2bcc18
iamsoojung <crystar2402@naver.com>
2025-10-31 14:16:50 +0900

    feat: 전역 에러 핸들러 추가

---
+++ src/main/java/com/example/todo/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.todo.common.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 유효성 검증 실패
+
+    // 커스텀 예외
+
+    // 기타 모든 예외
+}
